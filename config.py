2f51f2a7a7384b451bfb1c370b759877de44a453
Yiyang0519 <110144470+Yiyang0519@users.noreply.github.com>
2023-04-13 10:48:37 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "database-1.cordngpekchp.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "tanyiyang-employee"
+customregion = "us-east-1"
